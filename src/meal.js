cd2d4d753f1ea5ec9307fa46f4413611c8e524ba
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-08-28 17:55:20 -0400

    Complete meals.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`
+}
 
+function createMenuItem(menuItemName,price,type) {
+  return {
+    name: menuItemName,
+    price: price,
+    type: type
+  }
 }
 
 
+function addIngredients(item,ingredients) {
+  if(!ingredients.includes(item))
+  return ingredients.push(item)
+}
+
+var menuItem = createMenuItem()
+var initialPrice = menuItem.price
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+}
+
+function decreasePrice(initialPrice) {
+  var decreasePrice = initialPrice - (initialPrice *.10)
+  return decreasePrice
+}
+
+function createRecipe(title,ingredients,menuItemType) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
