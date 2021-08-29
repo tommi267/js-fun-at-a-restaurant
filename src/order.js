c2a9b38fc6ecb34b2b0227d7082fbf615590052e
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-08-29 15:36:02 -0400

    Complete order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(order,orderType) {
+  if(orderType.length < 3)
+  return orderType.push(order)
+
+}
+function refundOrder(orderNumber,orderType) {
+  for (var i = 0; i < orderType.length; i++) {
+    if (orderType[i].orderNumber === orderNumber)
+    orderType.splice(i,1)
+  }
+  return orderType
+}
+
+function listItems(orderType) {
+  var items = []
+  for (var i = 0; i < orderType.length; i++) {
+    items.push(orderType[i].item)
+  }
+  return items.join(", ")
+}
+function searchOrder(orderType,item) {
+  var trueCount = 0
+  for (var i = 0; i < orderType.length; i++) {
+    if (orderType[i].item.includes(item)){
+     trueCount ++
+   }
+}
+if (trueCount > 0){
+  return true
+}else{
+  return false
+}
+
+}
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+   takeOrder,
+   refundOrder,
+   listItems,
+   searchOrder
 }
