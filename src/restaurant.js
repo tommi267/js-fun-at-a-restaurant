cbcb19ed7f6230c51c898efcd6b6746771d9e144
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-08-29 18:51:46 -0400

    Complete resturant.js up to third test on removeMenuItem

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,41 @@
+function createRestaurant(name){
+  restaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: []
+    }
+  }
+  return restaurant;
+}
+function addMenuItem(restaurant,food){
+  if(food.type === 'lunch' && !restaurant.menus.lunch.includes(food)){
+  return restaurant.menus.lunch.push(food)
+}else if (food.type === 'breakfast'&& !restaurant.menus.breakfast.includes(food)){
+  return restaurant.menus.breakfast.push(food)
+}else if (food.type === 'dinner'&& !restaurant.menus.dinner.includes(food)){
+  return restaurant.menus.dinner.push(food)
+}
+}
+
+function removeMenuItem(restaurant,food,type){
+  for (var i = 0; i < restaurant.menus[type].length; i++) {
+    if(restaurant.menus[type][i].name === food){
+      restaurant.menus[type].splice(i,1)
+      var result = `No one is eating our ${food} - it has been removed from the ${type} menu!`
+      return result
+    }else {
+      var error = `Sorry we don't sell ${food}, try adding a new recipe!`;
+      return error;
+    }
+    }
+}
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
